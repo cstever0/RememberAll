bcd31f81d279a7d3704dcbabb542233e8c653600
Cory Stever <cstever@hotmail.com>
2023-06-13 15:03:56 -0500

    CommentForm Validators Updated

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, Length
 from app.models import Task
 
 
 class CommentForm(FlaskForm):
-    description = StringField("description", validators=[Optional(strip_whitespace=True)])
+    description = StringField("description", validators=[DataRequired(), Length(min=2, max=250, message="Please enter a comment between 2 and 250 characters")])
     user_id = IntegerField("user_id", validators=[DataRequired()])
     task_id = IntegerField("task_id", validators=[DataRequired()])
