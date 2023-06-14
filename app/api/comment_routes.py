9feae2811f1ee09b150a5646d84fb109d56ba87a
Cory Stever <cstever@hotmail.com>
2023-06-14 14:10:19 -0500

    Prepping Backend Label Routes

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import db, Task, Comment, Project
+from app.models import db, Comment
 from app.forms import CommentForm
 
 comment_routes = Blueprint('comments', __name__)
---
+++ app/forms/label_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, Optional, Length
+from app.models import Label
+
+
+class LabelForm(FlaskForm):
+    title = StringField("title", validators=[DataRequired(), Length(min=1, max=25, message="Please enter a name between 1 and 25 characters.")])
+    task_id = IntegerField("task_id", validators=[Optional(strip_whitespace=True)])
