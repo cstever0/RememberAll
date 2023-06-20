c85d4fdf7a29edeb1076186a8583478133121dae
Cory Stever <cstever@hotmail.com>
2023-06-20 16:05:31 -0500

    CSS Added

--- react-app/src/components/EditCommentModal/index.js
+++ react-app/src/components/EditCommentModal/index.js
@@ -10,8 +10,8 @@ function EditCommentModal({ comment }) {
     const [description, setDescription] = useState(comment.description);
     const [errors, setErrors] = useState([]);
     const { closeModal } = useModal();
-    console.log("errors", errors);
-    console.log("comment", comment);
+    // console.log("errors", errors);
+    // console.log("comment", comment);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -19,7 +19,7 @@ function EditCommentModal({ comment }) {
         comment.description = description;
 
         const data = await dispatch(updateOneComment(comment));
-        console.log("data", data)
+        // console.log("data", data)
 
         if (data) {
             setErrors(data);
@@ -36,10 +36,10 @@ function EditCommentModal({ comment }) {
                 encType="multipart/form-data"
                 className="edit-comment-modal-form"
             >
-                <div className="edit-comment-modal-error-container">
+                <div className="modal-error-container">
                     {errors?.map((error, idx) => <p key={idx}>{error}</p>)}
                 </div>
-                <div className="edit-comment-details">
+                <div className="edit-comment-modal-details">
                     <textarea
                         id="comment-modal-input"
                         value={description}
@@ -47,13 +47,23 @@ function EditCommentModal({ comment }) {
                         placeholder="Comment..."
                     />
                 </div>
-                <div className="edit-task-modal-submit-button">
-                    <button
-                        className="button-type"
-                        type="submit"
-                    >
-                        Save
-                    </button>
+                <div className="edit-comment-modal-submission-container">
+                    <div className="edit-comment-modal-cancel-button">
+                        <button
+                            className="button-type"
+                            onClick={closeModal}
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                    <div className="edit-comment-modal-submit-button">
+                        <button
+                            className="button-type"
+                            type="submit"
+                        >
+                            Save
+                        </button>
+                    </div>
                 </div>
             </form>
         </div>
